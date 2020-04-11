dbec0c89e118dbca837c29d4cb8b7db6dc191300
julialim <julialim@umich.edu>
2020-04-11 12:06:44 -0400

    infoWindow (still broken tho)

--- eatdwell/js/MapContainer.jsx
+++ eatdwell/js/MapContainer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
+import { Map, Marker, InfoWindow, GoogleApiWrapper } from 'google-maps-react';
 
 
 // ref: https://blog.vanila.io/writing-a-google-maps-react-component-fae411588a91
@@ -9,26 +9,75 @@ class MapContainer extends React.Component {
     constructor(props) {
       super(props);
 
+      this.state= {
+        showingInfoWindow: false,
+        activeMarker: {},
+        selectedPlace: {},
+      };
+
+    
+
     }
+
+    // NOTE: buggy. idk how to make it work in one function.
+    // onMarkerClick(){
+    //     console.log("clicked");
+    //     this.setState({
+    //         selectedPlace: props,
+    //         activeMarker: marker,
+    //         showingInfoWindow: true
+    //     });
+    // }
 
     renderChildren(){
         return (
+            
+                // create markers automatically somehow... this is just a dummy marker var for reference
+                <Marker
+                    name={'Location Name'}
+                    position={{lat: 42.291325, lng: -83.717495}}
+                    icon={{
+                        url: "/static/img/eatdwell.png",
+                        anchor: new google.maps.Point(20,15),
+                        scaledSize: new google.maps.Size(60,60)
+                    }} 
+                    
 
-            // create markers automatically somehow... this is just a dummy marker var for reference
-            <Marker
-                name={'Your position'}
-                position={{lat: 42.291325, lng: -83.717495}}
-                icon={{
-                    url: "/static/img/snowflake.png",
-                    anchor: new google.maps.Point(32,32),
-                    scaledSize: new google.maps.Size(64,64)
-                }} />
+                    // note: tried to make it into one function but it didn't work so pls help condense
+                    onClick={(props, marker, e) => {console.log("clicked");
+                    this.setState({
+                        selectedPlace: props,
+                        activeMarker: marker,
+                        showingInfoWindow: true
+                    });}}
+                       
+                /> //end of marker
+
+                // <InfoWindow
+                //     // create a popup infoWindow when clicking marker (just an example)
+                //     onOpen={this.windowHasOpened} // use to keep track of window states
+                //     onClose={this.windowHasClosed} 
+                //     visible={this.state.showingInfoWindow}
+                //     marker={this.state.activeMarker}>
+
+                //     <div>
+                //     <h1>{this.state.selectedPlace.name}</h1>
+                //     </div>
+                // </InfoWindow>
+
+            
+
         );
+
     }
 
+    
    
     render() {
+
         return (
+
+            
             < Map
               
                 google={this.props.google}
@@ -38,6 +87,22 @@ class MapContainer extends React.Component {
             >
                 {this.renderChildren()}
 
+                <InfoWindow
+                // create a popup infoWindow when clicking marker (just an example)
+                    onOpen={this.windowHasOpened} // use to keep track of window states
+                    onClose={this.windowHasClosed} 
+                    visible={this.state.showingInfoWindow}
+                    marker={this.state.activeMarker} //only get's one at a time
+                    //content="<img className='info' src='/static/img/eatdwell.png'></img>"
+                >
+                    
+                    <div id="infoWindow"> 
+                        <div>{this.state.selectedPlace.name}</div>
+                        <div><img className='info' src='static/img/test.png'></img></div>
+                    
+                    </div>
+                    
+                </InfoWindow>
             </Map>
         );
     }
